c1a3ac505d89e04dceaad10958ac935b7be0579a
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2026-07-19 10:13:25 +0530

    ORM initilization + Table creation + req.txt update

--- apps/api/app/db/database.py
+++ apps/api/app/db/database.py
@@ -1,19 +1,18 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from app.core.config import settings
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg://postgres:postgres@postgres:5432/asmo"
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    echo=True,
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False,
-)
+sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
 
-class Base(DeclarativeBase):
-    pass
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-
 from app.api.v1.router import api_router
+from contextlib import asynccontextmanager
+from app.util.init_db import create_tables
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    print("created db ini")
+    create_tables()
+    yield  # this is for the table generation
+
 
 app = FastAPI(
     title="ASMO API",
     version="1.0.0",
+    lifespan=lifespan
 )
 
 app.include_router(
@@ -18,3 +27,7 @@ def root():
     return {
         "message": "ASMO API Running",
     }
+
+@app.get("/health")
+def health_check():
+    return {"status" : "Running SMOOTH"}
